7d7541afb7129a8db4a946fe3e0b8ebb2f134ef9
Saransh Solanki <saransh.solanki@gmail.com>
2019-08-11 12:31:10 -0700

    new

--- events.js
+++ events.js
@@ -8,20 +8,22 @@ AFRAME.registerComponent('markerhandler', {
         // every click, we make our model grow in size :)
         Marker.addEventListener('click', function(ev, target){
 
-            elem.style.display = "block";
+            console.log("here");
             
+            elem.style.display = "block";
+
             // elem.requestFullscreen();
             // elem.mozRequestFullScreen();
-            elem.webkitRequestFullscreen();
+            // elem.webkitRequestFullscreen();
 
             // elem.msRequestFullscreen();
 
 
-            const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
-            if (aEntity && intersectedElement === aEntity) {
-                const scale = aEntity.getAttribute('scale');
-                Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
-                aEntity.setAttribute('scale', scale);
-            }
+            // const intersectedElement = ev && ev.detail && ev.detail.intersectedEl;
+            // if (aEntity && intersectedElement === aEntity) {
+            //     const scale = aEntity.getAttribute('scale');
+            //     Object.keys(scale).forEach((key) => scale[key] = scale[key] + 1);
+            //     aEntity.setAttribute('scale', scale);
+            // }
         });
 }});
